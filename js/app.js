19f651c6cacca1d85b9fc25b5cf6a2e5e74a45a3
rashedsabbir <rashedulhasan1996@gmail.com>
2022-03-11 21:27:51 +0600

    username and comment error fixed

--- js/app.js
+++ js/app.js
@@ -28,13 +28,7 @@ const reportPost = (id) => {
 
 const displayContent = (text) => {
   return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
-  // let overView;
-  // if (text.length > 30) {
-  //   return text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
-  // }
-  // else {
-  //   return text;
-  // }
+
 };
 
 const switchTab = (id) => {
@@ -127,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post?.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post?.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
