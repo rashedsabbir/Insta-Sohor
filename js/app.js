fc494915c04a6217de1c349f7da4479bf023fe6b
rashedsabbir <rashedulhasan1996@gmail.com>
2022-03-11 20:43:40 +0600

    text error fixed

--- js/app.js
+++ js/app.js
@@ -27,7 +27,14 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  // let overView;
+  // if (text.length > 30) {
+  //   return text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  // }
+  // else {
+  //   return text;
+  // }
 };
 
 const switchTab = (id) => {
